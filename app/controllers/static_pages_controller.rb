b6460c7d530b9e4e006487c42b1c219671f94d05
lbackman <lucasbackman@gmail.com>
2023-02-09 17:17:26 +0200

    Move API call to controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
-    @photos = flickr.photos.search(user_id: '197629877@N03', per_page: 10)
+    @photos = flickr.photos.search(user_id: '197629877@N03', per_page: 10).map { |photo| Flickr.url(photo) }
   end
 end
