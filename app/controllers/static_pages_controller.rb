60695e500ee6606301cf027cc93c497848ad0599
lbackman <lucasbackman@gmail.com>
2023-02-09 18:08:13 +0200

    Implement search function
    Allows the user to search for a Flickr id and view its public photos.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
-    @photos = flickr.photos.search(user_id: '197629877@N03', per_page: 10).map { |photo| Flickr.url(photo) }
+    @user_id = params[:user_id]
+    if @user_id
+      begin
+        @photos = flickr.photos.search(user_id: @user_id, per_page: 10).map { |photo| Flickr.url(photo) }
+      rescue Flickr::FailedResponse => e
+        @error = "Unknown user id: #{@user_id}"
+      end
+    end
   end
 end
